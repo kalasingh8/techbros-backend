3786e581bc94346975bc15126a74cb5441610b1c
kalasingh8 <s379095@email.com>
2026-06-14 11:22:18 +0530

    Fix console.log template literal

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server running on port );
+  console.log(`Server running on port ${PORT}`);
 });
